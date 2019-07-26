543b6f1f12c5f06c94589c31f74fda55acf8a1bd
Colin DeCarlo <c.decarlo@vehikl.com>
2019-07-26 13:16:47 -0400

    add the first rule

--- src/Cell.js
+++ src/Cell.js
@@ -1,11 +1,27 @@
 class Cell {
+  constructor() {
+    this.animated = false;
+    this.neighbours = [];
+  }
 
   static dead() {
     return new Cell;
   }
 
+  tick() {
+    this.animate();
+  }
+
   isDead() {
-    return true;
+    return this.animated == false;
+  }
+
+  animate() {
+    this.animated = true;
+  }
+
+  addNeighbours(newNeighbours) {
+    this.neighbours = [...this.neighbours, ...newNeighbours];
   }
 }
 
--- src/__tests__/Cell.spec.js
+++ src/__tests__/Cell.spec.js
@@ -1,9 +1,26 @@
 import Cell from "../Cell";
 
 describe("Cell", () => {
-  it('dead cells can be created', () => {
+  it('is dead when it is created', () => {
     const cell = Cell.dead();
 
     expect(cell.isDead()).toBe(true);
+  });
+
+  it('can be animated', () => {
+    const cell = Cell.dead();
+
+    cell.animate();
+
+    expect(cell.isDead()).toBe(false);
+  });
+
+  it('becomes animated when it has 3 live neighbors', () => {
+    const cell = Cell.dead();
+    cell.addNeighbours(Array.from({ length: 3}).map(_ => Cell.dead().animate()));
+
+    cell.tick();
+
+    expect(cell.isDead()).toBe(false);
   })
 });
